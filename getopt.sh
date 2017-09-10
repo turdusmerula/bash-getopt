11f74655ea9d8412f2aa18c68f30e7b9bf1c2e60
Sebastien Besombes <sebastien.besombes@gmail.com>
2017-09-10 12:01:33 +0200

    Update getopt.sh

--- getopt.sh
+++ getopt.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# 
+# This file is part of the bash-getopt distribution (https://github.com/turdusmerula/bash-getopt).
+# Copyright (c) 2015 Sebastien Besombes.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
 version=1.0.0
 getopt_option_num=0
 #getopt_shorts[]: short names
